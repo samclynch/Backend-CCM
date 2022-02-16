23f1ab43991f71b9ad05c6cc94ffc328ef83660b
Sam Lynch <samdawkins1998@gmail.com>
2022-02-16 11:54:28 -0500

    recommit for heroku

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :jobs
   post '/login', to: 'sessions#login'
   get '/authorized_user', to: 'user#show'
-  get
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
